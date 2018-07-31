37c041946e10d626ef287dbfc3ae0bdf5bd378bc
Francisco Júnior <francisco.junior@se.df.gov.br>
2018-07-31 14:21:23 -0300

    inclusão de operações matematicas, comentários e concatenação de strings.

--- teste.rb
+++ teste.rb
@@ -1,2 +1,22 @@
-puts "Teste arquivo ruby"
-puts 10
+puts "Teste cálculos em ruby"
+puts ""
+puts "10 + 5 = #{10 + 5}" #Para concatenar uma string com um valor deve-se utilizar #{valor}
+puts "8 - 4 = #{8 - 4}"
+puts "4 * 4 = #{4 * 4}"
+puts "24 / 6 = #{24 / 6}"
+puts "(5 + 5 + 10) / 10 = #{(5 + 5 + 10) / 10}"
+puts "10 / 5 + 5 = #{10 / 5 + 5}"
+puts ""
+puts "Maneira de repetir um texto duas vezes" * 2
+puts 10 + 10
+puts "10"+"10"
+puts "10"*4
+puts 10 * 4
+=begin
+comentário em várias linhas
+****************************
+****************************
+****************************
+****************************
+****************************
+=end
